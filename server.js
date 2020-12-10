c616e64eb1e9595f622181dc5b53cef2ea7884c6
Dannette Tejeda <dannettetejeda@gmail.com>
2020-12-09 21:54:34 -0500

    deploy

--- server.js
+++ server.js
@@ -30,13 +30,4 @@ mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/workout`,
   useCreateIndex: true
 });
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://dit1091:>@cluster0.bxcpm.mongodb.net/<dbname>?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-
-
 app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`));
