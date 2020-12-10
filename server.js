4fc11d502911c58a7170bd4978b40250ecc2e653
Dannette Tejeda <dannettetejeda@gmail.com>
2020-12-09 21:24:15 -0500

    deply

--- server.js
+++ server.js
@@ -22,11 +22,21 @@ htmlRoutes(app);
 apiRoutes(app);
 
 
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/workout`, {
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/workout`, 
+{
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
   useCreateIndex: true
 });
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://dit1091:>@cluster0.bxcpm.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+
 
 app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`));
